177ef5421310a52af7447a244eb38c248e0c5c4a
khandelwalpranav05 <khandelwalpranav@rocketmail.com>
2018-10-22 03:07:12 +0530

    testing

--- test.py
+++ test.py
@@ -13,11 +13,8 @@ classifier = load_model('Trained_model.h5')
 def predictor():
 	test_image = image.load_img('1.png', target_size=(64, 64))
 	test_image = image.img_to_array(test_image)
-	print(test_image.shape)
 	test_image = np.expand_dims(test_image,axis=0)
-	print(test_image.shape)
 	result = classifier.predict(test_image)
-	print(result.shape)
 
 	if result[0][0] == 1:
 		return 'A'
@@ -74,3 +71,50 @@ def predictor():
 	
 
 cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("Trackbars")
+
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 36, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 198, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 229, 255, nothing)
+
+img_text = ''
+
+while True:
+    ret, frame = cam.read()
+    frame = cv2.flip(frame,1)
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+
+
+    img = cv2.rectangle(frame, (425,100),(625,300), (0,255,0), thickness=2, lineType=8, shift=0)
+
+    lower_blue = np.array([l_h, l_s, l_v])
+    upper_blue = np.array([u_h, u_s, u_v])
+    imcrop = img[102:298, 427:623]
+    hsv = cv2.cvtColor(imcrop, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    cv2.putText(frame, img_text, (30, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 255, 0))
+    cv2.imshow("test", frame)
+    cv2.imshow("mask", mask)
+
+    img_name = "1.png"
+    save_img = cv2.resize(mask, (image_x, image_y))
+    cv2.imwrite(img_name, save_img)
+
+    img_text = predictor()
+        
+    
+    if cv2.waitKey(1) == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
